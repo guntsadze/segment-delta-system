90bca8de3b171912451b31b48a97d99047d96eea
guntsadze <dimitri.guncadze@gmail.com>
2026-05-15 19:13:13 +0400

    [ADD]: logo

--- apps/ui/app/layout.tsx
+++ apps/ui/app/layout.tsx
@@ -1,8 +1,17 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Segments Live Data",
+  description: "Advanced Customer Segmentation Engine",
+  icons: {
+    icon: "/ico.svg",
+  },
+};
 
 export default function RootLayout({
   children,
