7e2afb85c9da9d5b6e3cbc16c593d41c91826478
guntsadze <dimitri.guncadze@gmail.com>
2026-05-15 19:57:20 +0400

    [CONFIGURATION]: simple optimization added in next.config.ts

--- apps/ui/next.config.ts
+++ apps/ui/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  compress: true,
+  reactCompiler: true,
 };
 
 export default nextConfig;
