201e55c61ce9bfe0e4f323bfe64e9458c6d41615
guntsadze <dimitri.guncadze@gmail.com>
2026-05-15 19:21:12 +0400

    [TEST]: build test

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
   const port = process.env.PORT ?? 5000;
   await app.listen(port, '0.0.0.0');
 
-  console.log(`🚀 Server is running on port ${port}`);
+  // console.log(`🚀 Server is running on port ${port}`);
 }
 bootstrap();
